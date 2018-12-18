90a05928b36c9c0d7065c539838d7938974d2292
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2018-12-18 15:21:36 +0100

    Correct encoding in SQL schema

--- setup.sql
+++ setup.sql
@@ -4,14 +4,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `dead_composers` (
   `id` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `birth_day` date DEFAULT NULL,
   `death_day` date NOT NULL,
-  `public_domain_day` date not null,
-  `public_domain_years` int(11) not null,
-  `nationality` varchar(2) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
-  `source_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+  `public_domain_day` date NOT NULL,
+  `public_domain_years` int(11) NOT NULL,
+  `nationality` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `source_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 ALTER TABLE `dead_composers`
   ADD PRIMARY KEY (`id`);
